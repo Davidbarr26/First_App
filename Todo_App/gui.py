0850ff186358a0a4a26f0bd317af5a1542c44ac9
Davidbarrios1126 <jdbarrios02@gmail.com>
2024-12-26 21:33:30 -0500

    Program completed

--- Todo_App/gui.py
+++ Todo_App/gui.py
@@ -1,6 +1,10 @@
 import functions
 import FreeSimpleGUI as UI
+import time
 
+UI.theme("DarkGrey16")
+
+date = UI.Text('', key='datetime')
 label = UI.Text("Type in a to-do")
 input_box = UI.InputText(tooltip="Enter a to-do to the list: ", key='todo')
 add_button = UI.Button("Add")
@@ -12,21 +16,19 @@ exit_button = UI.Button("Exit")
 footer = UI.Text(functions.display_copyright())
 
 window = UI.Window('To-Do App',
-                   layout=[[label],
+                   layout=[[date],
+                           [label],
                            [input_box],
                            [add_button,
-                            show_button,
                             edit_button,
-                            complete_button,
-                            exit_button],
+                            complete_button],
                            [list_box],
+                           [exit_button],
                            [footer]],
                    font=('Helvetica', 12))
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+    event, values = window.read(timeout=10)
+    window['datetime'].update(value=time.strftime("%A, %d, %b, %Y \nTime: %H:%M:%S (%Z)"))
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -34,14 +36,29 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
         case "Edit":
-            edit_todo = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                edit_todo = values['todos'][0]
+                new_todo = values['todo']
 
-            todos = functions.get_todos()
-            index = todos.index(edit_todo)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(edit_todo)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                UI.popup("Please select an item.", font=("Arial", 15))
+        case 'Complete':
+            try:
+                complete_todo = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(complete_todo)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                UI.popup("Please select an item.", font=("Arial", 15))
+        case 'Exit':
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
 
